219a55b89211aea490b069928827757691c26a25
Marcos Vinicius Pereira <marcosvinipereira4@gmail.com>
2026-04-26 23:13:00 -0300

    adicionando audio para ataque melee, textura área teletransporte abertura

--- Assets/Scripts/Attacks/MeleeAttack.cs
+++ Assets/Scripts/Attacks/MeleeAttack.cs
@@ -4,10 +4,15 @@ public class MeleeAttack : MonoBehaviour
 {
     public float damage = 25f;
     public float lifeTime = 0.2f;
+    private AudioSource attackSoundEffect;
 
     void Start()
     {
         Destroy(gameObject, lifeTime);
+
+        GameObject audio = GameObject.Find("AudioAttack");
+        if (audio != null)
+            attackSoundEffect = audio.GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -15,7 +20,9 @@ public class MeleeAttack : MonoBehaviour
         if (other.CompareTag("Player")) return;
 
         Health health = other.GetComponent<Health>();
-        if (health != null)
+        if (health != null) {
             health.TakeDamage(damage);
+            attackSoundEffect.Play();
+        }
     }
 }
